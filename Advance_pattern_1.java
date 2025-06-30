1359896691b823ecd73f2000ec19e516a4390da7
Ibne-suny <mdivnesuny2021@gmail.com>
2025-06-30 21:18:34 +0600

    advance pattern

---
+++ Advance_pattern_1.java
@@ -0,0 +1,41 @@
+package Advance_pattern_question_in_java;
+
+public class Advance_pattern_1 {
+    public static void main(String[] args) {
+        int n=4;
+
+      for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+
+        int space=2*(n-i);
+        for(int j=1;j<=space;j++){
+            System.out.print(" ");
+        }
+
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+      }
+//2nd Half........
+
+
+      for(int i=n;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+
+        int space=2*(n-i);
+        for(int j=1;j<=space;j++){
+            System.out.print(" ");
+        }
+
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+      }
+    }
+}
